6c5989eb90ee7cee2cecdd6018ece8c87771acb2
jhonntantb <jhonntantb@gmail.com>
2022-06-04 21:49:56 -0500

    congid main and module app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ProductsModule } from './products/products.module';
+import { CategoryModule } from './category/category.module';
+import { Product } from './products/model/product.model';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+  }),
+    SequelizeModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'mysql',
+        port:3306,
+        host: configService.get<string>('MY_SQL_HOST'),
+        username: configService.get<string>('MY_SQL_USERNAME'),
+        password: configService.get<string>('MY_SQL_PASSWORD'),
+        database: configService.get<string>('MY_SQL_DATABASE'),
+        autoLoadModels: false,
+        synchronize: true,
+        ssl: false,
+        models:[Product]
+      }),
+    }),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api')
+  const options = new DocumentBuilder()
+        .setTitle('Bsale Backend API')
+        .setDescription('Bsale query monolithic')
+        .setVersion('0.1')
+        .addBearerAuth()
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('docs', app, document);
   await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
